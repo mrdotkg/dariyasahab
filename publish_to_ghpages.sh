755b6d17ca774166f2c525b6fa3bcfd7f06bf359
Kumar Gaurav <grv.rkg@gmail.com>
2020-02-05 02:33:38 +0530

    base url

--- publish_to_ghpages.sh
+++ publish_to_ghpages.sh
@@ -7,23 +7,23 @@ then
     exit 1;
 fi
 
-echo "Deleting old publication"
+echo "** Deleting old publication"
 rm -rf public
 mkdir public
 git worktree prune
 rm -rf .git/worktrees/public/
 
-echo "Checking out gh-pages branch into public"
+echo "** Checking out gh-pages branch into public"
 git worktree add -B gh-pages public origin/gh-pages
 
-echo "Removing existing files"
+echo "** Removing existing files"
 rm -rf public/*
 
-echo "Generating site"
+echo "** Generating site"
 hugo
 
-echo "Updating gh-pages branch"
+echo "** Updating gh-pages branch"
 cd public && git add --all && git commit -m "Publishing to gh-pages (publish.sh)"
 
-echo "Pushing to github"
+echo "** Pushing to github"
 git push --all
